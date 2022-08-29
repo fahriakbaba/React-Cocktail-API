ec607d0d35d773475d7e3a1204ce0e7e6496848c
Fahri Akbaba <fahriakbaba@gmail.com>
2022-08-29 22:54:21 +0300

    styled SingleCocktail component

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,7 +5,7 @@ function Error() {
     <section className="error--page">
         <h3 className="error-title">404 error</h3>
         <p className="error--info">This page doesn't exist.</p>
-        <button><Link to="/">Back Home</Link></button>
+        <button className="btn btn-details"><Link to="/" className="cocktail--link">Back Home</Link></button>
     </section>
   )
 }
--- src/pages/SingleCocktail.jsx
+++ src/pages/SingleCocktail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
-
+import { Link } from "react-router-dom";
 
 function SingleCocktail() {
   const { id } = useParams();
@@ -11,7 +11,6 @@ function SingleCocktail() {
   React.useEffect(() => {
     getCocktail(id);
   }, [id]);
-
 
   const getCocktail = async (id) => {
     setLoading(true);
@@ -59,18 +58,41 @@ function SingleCocktail() {
 
   console.log(data);
 
-  if(loading) {
-    return <Loading />
+  if (loading) {
+    return <Loading />;
   }
 
-
   return (
-  <section>
-    <img src={data.image} alt={data.strGlass} />
-  </section>
+    <section className="single-cocktail-container">
+      <div className="single-cocktail-image">
+        <img src={data.image} alt={data.strGlass} />
+      </div>
+      <div className="single-cocktail-info">
+        <button className="btn btn-details">
+          <Link to={"/"} className="cocktail--link">Back Home</Link>
+        </button>
+        <h4>
+          <span>Name:</span> <span>{data.strDrink}</span>
+        </h4>
+        <h4>
+          <span>Category:</span> <span>{data.strCategory}</span>
+        </h4>
+        <h4>
+          <span>Info:</span> <span>{data.strAlcoholic}</span>
+        </h4>
+        <h4>
+          <span>Glass:</span> <span>{data.strGlass}</span>
+        </h4>
+        <h4>
+          <span>Instructons:</span> <span>{data.strInstructions}</span>
+        </h4>
+        <h4>
+          <span>Ingredients:</span>{" "}
+          <span>{data.ingredients.map((item) => " " + item + " ")}</span>{" "}
+        </h4>
+      </div>
+    </section>
   );
 }
 
 export default SingleCocktail;
-
- 
